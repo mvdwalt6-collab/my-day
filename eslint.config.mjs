614d9ff591107bcf018a40206cbe557576c8e4a8
mvdwalt6-collab <mvdwalt6@gmail.com>
2026-08-04 17:46:56 +0200

    chore: switch lint to eslint cli

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,10 @@
 import nextVitals from "eslint-config-next/core-web-vitals";
 
-const config = [...nextVitals];
+const config = [
+	{
+		ignores: ["legacy/**"],
+	},
+	...nextVitals,
+];
 
 export default config;
